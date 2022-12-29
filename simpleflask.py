7cf9c6a42d7336676ccc514e742c80776a8e6b6c
KHSstorage <gurtn0512@gmail.com>
2022-12-29 17:13:12 +0900

    flask

--- simpleflask.py
+++ simpleflask.py
@@ -1,6 +1,19 @@
-a=10
-b=20
-c=a+b
-print(c)
+#필요한 모듈을 단계적으로 import
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+# 테스트 기능
+@app.route('/hello')
+def hello_Flask():
+    return 'Hello, Flask'
 
-print('Hello Flask')
+@app.route('/hi')
+def hi_Flask():
+    return 'Hi, Flask'
+ 
+if __name__ == '__main__':
+    app.run(
+    host="0.0.0.0",
+    port=7777,
+    debug=True)
